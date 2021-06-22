4222d53406e590d8cbb2acde48f6831bdab56d18
Jason Zhuang <zhuangkaizhi@gmail.com>
2021-06-22 10:24:38 +1000

    design a comprehensive header
    writing design guide

---
+++ src/components/header/Header.js
@@ -0,0 +1,29 @@
+//1.The main elements of a website header usually include:
+//Logo or brand identifier
+//Navigational links and menus
+//Hidden Navigation - Hidden navigation is when navigational links appear on click or hover.
+//A popular hidden navigation solution is the hamburger button or collapsed menu icon.It consists of horizontal lines that represent the menu. Another popular hidden navigation method is the use of drop - down menus.
+//Social Media buttons
+//Contact information
+//Language options
+//Search field
+//Call To Action Buttons
+//Login field
+//Shopping Cart
+//Notifications
+
+//2.We need to consider header css as follows:
+//Font
+//Colors
+//Padding - Whitespace between elements helps visitors take in the information faster. Elements placed close to each other give the idea that they are related.
+//Alignment
+//Margin
+
+
+//3.We need to consider header effects as follows:
+//Parallax Effects
+//Fixed or Sticky Header
+
+
+//4. Goog Examples Header Design:
+//https://awakeboards.com/
